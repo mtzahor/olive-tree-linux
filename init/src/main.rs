da0b100f3721d68391aa65a6b7147721d1cfadc9
mtzahor <mtzahor@gmail.com>
2026-08-26 12:30:49 +0300

    Olive Tree Linux 0.0.4

--- init/src/main.rs
+++ init/src/main.rs
@@ -132,7 +132,7 @@ fn shutdown(shell_pid: libc::pid_t) -> ! {
 fn main() {
     println!("================================");
     println!("      Olive Tree Linux");
-    println!("           0.0.3");
+    println!("           0.0.4");
     println!("================================");
 
     unsafe {
